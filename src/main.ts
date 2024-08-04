04ab27453ecf37d295accdfad828ddb38f4d2d18
amitsafi45 <safiamit45@gmail.com>
2024-08-04 16:49:05 +0545

    add: config service

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { ConfigService } from '@nestjs/config';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000, () => {
+  const app = await NestFactory.create(AppModule, {
+    logger: ['debug', 'error', 'log', 'warn'],
+  });
+  const httpAdapter = app.get(HttpAdapterHost);
+  const PORT_NUMBER = app.get(ConfigService).get('PORT');
+  await app.listen(PORT_NUMBER, () => {
     console.log(`Server Listening At Port`);
   });
 }
