bd9377e03e6b9272db2f6c868874704a228b25f1
amitsafi45 <safiamit45@gmail.com>
2024-08-06 04:56:10 +0545

    add: character length enum

--- src/constants/enum.ts
+++ src/constants/enum.ts
@@ -13,3 +13,9 @@ export enum Environment {
   Development = 'Development',
   Production = 'Production',
 }
+
+export enum CharacterLength {
+  ONE_HUNDRED = 100,
+  TWO_HUNDRED = 200,
+  TWO_HUNDRED_FIFTY_FIVE = 255,
+}
