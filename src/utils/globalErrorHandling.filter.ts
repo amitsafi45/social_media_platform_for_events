d12d74c7ecaae27dfb6845f391d1f363c19de0dd
amitsafi45 <safiamit45@gmail.com>
2024-08-06 02:58:10 +0545

    fix: handle database query failed exception

--- src/utils/globalErrorHandling.filter.ts
+++ src/utils/globalErrorHandling.filter.ts
@@ -6,28 +6,35 @@ import {
   HttpStatus,
 } from '@nestjs/common';
 import { HttpAdapterHost } from '@nestjs/core';
+import { QueryFailedError } from 'typeorm';
 
-@Catch(HttpException)
+@Catch()
 export class GlobalErrorHandlingFilter implements ExceptionFilter {
   constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
 
-  catch(exception: HttpException, host: ArgumentsHost): void {
+  catch(exception: unknown, host: ArgumentsHost): void {
     const { httpAdapter } = this.httpAdapterHost;
     const ctx = host.switchToHttp();
-    const httpStatus =
-      exception instanceof HttpException
-        ? exception.getStatus()
-        : HttpStatus.INTERNAL_SERVER_ERROR;
+    const request = ctx.getRequest<Request>();
+    const response = ctx.getResponse<Response>();
+
+    let httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+    let message = 'Internal server error';
+
+    if (exception instanceof HttpException) {
+      httpStatus = exception.getStatus();
+      message = exception.message;
+    } else if (exception instanceof QueryFailedError) {
+      httpStatus = 500;
+      message = 'Database query failed: ' + exception.message;
+    }
 
     const responseBody = {
       statusCode: httpStatus,
       success: false,
-      message: exception.message,
+      message,
     };
-    //   timestamp: new Date().toISOString(),
-    //   path: httpAdapter.getRequestUrl(ctx.getRequest()),
-    //   ip:httpAdapter.getRequestHostname(ctx.getRequest()),
 
-    httpAdapter.reply(ctx.getResponse(), responseBody, httpStatus);
+    httpAdapter.reply(response, responseBody, httpStatus);
   }
 }
