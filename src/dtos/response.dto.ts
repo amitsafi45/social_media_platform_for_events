d2b03b9076cb5521150df4802ce5d56d47a7809a
amitsafi45 <safiamit45@gmail.com>
2024-08-06 03:02:04 +0545

    add: design DTO for define return type & for swagger doc

---
+++ src/dtos/response.dto.ts
@@ -0,0 +1,61 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class SuccessResponseDTO {
+  @ApiProperty({
+    description: 'HTTP status code',
+    example: 201,
+  })
+  statusCode: number;
+
+  @ApiProperty({
+    description: 'Success status of the response',
+    example: true,
+  })
+  success: boolean;
+
+  @ApiProperty({
+    description: 'Success message',
+    example: 'User registered successfully',
+  })
+  message: string;
+}
+
+export class ErrorResponseDTO {
+  @ApiProperty({
+    description: 'HTTP status code',
+    example: 400,
+  })
+  statusCode: number;
+
+  @ApiProperty({
+    description: 'Success status of the response',
+    example: false,
+  })
+  success: boolean;
+
+  @ApiProperty({
+    description: 'Error message describing what went wrong',
+    example: 'error message',
+  })
+  message: string;
+}
+
+export class DatabaseQueryFailedResponseDTO {
+  @ApiProperty({
+    description: 'HTTP status code',
+    example: 500,
+  })
+  statusCode: number;
+
+  @ApiProperty({
+    description: 'Status of the response',
+    example: false,
+  })
+  success: boolean;
+
+  @ApiProperty({
+    description: 'Error message describing what went wrong',
+    example: 'Database query failed due to invalid input.',
+  })
+  message: string;
+}
